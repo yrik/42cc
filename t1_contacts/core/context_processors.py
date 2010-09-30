21d9d6accea544db336804f4370b4a9181423baf
Iurii Kriachko <iurii.kriachko@gmail.com>
2010-09-30 19:53:22 +0300

    conflict resolution 2

--- t1_contacts/core/context_processors.py
+++ t1_contacts/core/context_processors.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-def add_settings(request):
-    from django.conf import settings
-    return {'settings':settings}
-=======
 dd_settings(request):
     from django.conf import settings
         return {'settings':settings}
 
 
->>>>>>> t3_middleware
