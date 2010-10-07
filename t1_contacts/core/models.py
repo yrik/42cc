e61048acf9b7dedf1457a3af6bde4da217a0db0b
Iurii Kriachko <iurii.kriachko@gmail.com>
2010-10-07 21:09:13 +0300

    ticket 11:ajax form

--- t1_contacts/core/models.py
+++ t1_contacts/core/models.py
@@ -54,6 +54,7 @@ class PersonForm(forms.Form):
             js = (
                 '/static/js/jquery-1.4.2.min.js',
                 '/static/js/jquery.form.js',
+                '/static/js/custom.js',
             )
 
 
--- t1_contacts/core/views.py
+++ t1_contacts/core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from core.models import Person, PersonForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.utils import simplejson
 
 def index(request):
 
@@ -23,9 +24,6 @@ def first10items(request):
 
     """
     >>> from django.test import Client
-    >>> from django.core.urlresolver import reverse
-    >>> client = Client()
-
     >>> response = client.get('/first10items')
     >>> response.status_code
     200
@@ -73,10 +71,23 @@ def add_person(request):
     if request.method == 'POST': # If the form has been submitted...
         form = PersonForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
-            newPerson = Person(name=form.cleaned_data['name'],bio=form.cleaned_data['bio'],contacts=form.cleaned_data['contacts'],surname=form.cleaned_data['surname'],birth_date=form.cleaned_data['birth_date'])
+            newPerson = Person(name=form.cleaned_data['name'],bio=form.cleaned_data['bio'],
+                        contacts=form.cleaned_data['contacts'],surname=form.cleaned_data['surname'],birth_date=form.cleaned_data['birth_date'])
             newPerson.save()
-
+    
+        if request.is_ajax():
+            rdict = {'bad':'false'}
+            if not form.is_valid():
+                rdict.update({'bad':'true'})
+                d={}
+                for e in form.errors.iteritems():
+                    d.update({e[0]:unicode(e[1])})
+                rdict.update({'errs': d  })
+            json = simplejson.dumps(rdict, ensure_ascii=False)
+            return HttpResponse( json, mimetype='application/javascript')
+        else:
             return HttpResponseRedirect('/') # Redirect after POST
+   
     else:
         form = PersonForm() # An unbound form
 
@@ -116,11 +127,25 @@ def edit_person(request):
             p.surname=form.cleaned_data['surname']
             p.birth_date=form.cleaned_data['birth_date']
             p.save()
-
+        
+        if request.is_ajax():
+            rdict = {'bad':'false'}
+            if not form.is_valid():
+                rdict.update({'bad':'true'})
+                d={}
+                for e in form.errors.iteritems():
+                    d.update({e[0]:unicode(e[1])})
+                rdict.update({'errs': d  })
+            json = simplejson.dumps(rdict, ensure_ascii=False)
+            return HttpResponse( json, mimetype='application/javascript')
+        else:
             return HttpResponseRedirect('/') # Redirect after POST
     else:
-        p = Person.objects.get(name="Iurii", surname="Kriachko",id=1)
-        form = PersonForm({'id':p.id,'name':p.name,'surname':p.surname,'bio':p.bio,'contacts':p.contacts,'birth_date':p.birth_date})
+        try:
+            p = Person.objects.get(name="Iurii", surname="Kriachko",id=1)
+            form = PersonForm({'id':p.id,'name':p.name,'surname':p.surname,'bio':p.bio,'contacts':p.contacts,'birth_date':p.birth_date})
+        except:
+            return HttpResponseRedirect('/')
 
     return render_to_response('edit_person.html', {
         'form': form,
