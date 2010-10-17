7a5e242ba643d2e7a2ce0967a2cf141c6b9b03c5
Iurii Kriachko <iurii.kriachko@gmail.com>
2010-10-17 17:46:11 +0300

    Class Log is extended

--- t1_contacts/core/models.py
+++ t1_contacts/core/models.py
@@ -19,6 +19,8 @@ class Log(models.Model):
         (2, 'high'),
     )
     content = models.TextField(null=True, blank=True)
+    date = models.DateTimeFieldField(null=True, blank=True, auto_now=True)
+    content = models.TextField(null=True, blank=True)
     priority = models.IntegerField(choices=PC, default=0)
 
     def __unicode__(self):
@@ -56,7 +58,7 @@ class Person(models.Model):
 
 def delete_callback(sender, instance, signal, *args, **kwargs):
     l = Log(content="object '%s' is deleted" % instance)
-    Log.save(l)
+    l.save()
 
 
 def save_callback(sender, instance, signal, *args, **kwargs):
@@ -65,7 +67,7 @@ def save_callback(sender, instance, signal, *args, **kwargs):
             l = Log(content="object '%s' is created" % instance)
         else:
             l = Log(content="object '%s' is edited" % instance)
-        Log.save(l)
+        l.save()
 
 
 post_save.connect(save_callback)
