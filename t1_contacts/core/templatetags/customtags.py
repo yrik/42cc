676987db73ed96c3dda52bf44c5e0ec55f2a7648
Iurii Kriachko <iurii.kriachko@gmail.com>
2010-10-20 16:50:06 +0300

    ticket 8: tempale error is replaced by returning empty string

--- t1_contacts/core/templatetags/customtags.py
+++ t1_contacts/core/templatetags/customtags.py
@@ -5,10 +5,8 @@ register = template.Library()
 
 @register.simple_tag
 def edit_link(obj=''):
-    if not obj:
-        raise template.TemplateSyntaxError, "'edit_link' tag requires object as argument"
     try:
         url = reverse('admin:%s_%s_change' %(obj._meta.app_label,  obj._meta.module_name),  args=[obj.id] )
         return u'<a href="%s">Edit %s</a>' %(url,  obj.__unicode__())
     except:
-        raise template.TemplateSyntaxError, "'edit_link' argument need to has _meta.app_label, obj._meta.module_name, obj.id properties"
+        return ''
